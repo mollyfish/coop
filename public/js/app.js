9b2e35234a8c903c80aefb2e3cc3f60c091ebc7b
Molly Kent <kent.molly@gmail.com>
2015-07-02 19:06:10 -0700

    draft front page

--- public/js/app.js
+++ public/js/app.js
@@ -13,36 +13,48 @@ $(function() {
     if ($(toPop).attr('id') === 'caro-1') {
       $('#caro-2').removeClass('backup').addClass('diva');
       $('#caro-1').removeClass('diva').addClass('backup');
+      $('#caro-2-link').removeClass('backup').addClass('diva');
+      $('#caro-1-link').removeClass('diva').addClass('backup');
       $('#pos1').removeClass('diva');
       $('#pos2').addClass('diva');
     }
     if ($(toPop).attr('id') === 'caro-2') {
       $('#caro-3').removeClass('backup').addClass('diva');
       $('#caro-2').removeClass('diva').addClass('backup');
+      $('#caro-3-link').removeClass('backup').addClass('diva');
+      $('#caro-2-link').removeClass('diva').addClass('backup');
       $('#pos2').removeClass('diva');
       $('#pos3').addClass('diva');
     }
     if ($(toPop).attr('id') === 'caro-3') {
       $('#caro-4').removeClass('backup').addClass('diva');
       $('#caro-3').removeClass('diva').addClass('backup');
+      $('#caro-4-link').removeClass('backup').addClass('diva');
+      $('#caro-3-link').removeClass('diva').addClass('backup');
       $('#pos3').removeClass('diva');
       $('#pos4').addClass('diva');
     }
     if ($(toPop).attr('id') === 'caro-4') {
       $('#caro-5').removeClass('backup').addClass('diva');
       $('#caro-4').removeClass('diva').addClass('backup');
+      $('#caro-5-link').removeClass('backup').addClass('diva');
+      $('#caro-4-link').removeClass('diva').addClass('backup');
       $('#pos4').removeClass('diva');
       $('#pos5').addClass('diva');
     }
     if ($(toPop).attr('id') === 'caro-5') {
       $('#caro-6').removeClass('backup').addClass('diva');
       $('#caro-5').removeClass('diva').addClass('backup');
+      $('#caro-6-link').removeClass('backup').addClass('diva');
+      $('#caro-5-link').removeClass('diva').addClass('backup');
       $('#pos5').removeClass('diva');
       $('#pos6').addClass('diva');
     } 
     if ($(toPop).attr('id') === 'caro-6') {
       $('#caro-1').removeClass('backup').addClass('diva');
       $('#caro-6').removeClass('diva').addClass('backup');
+      $('#caro-1-link').removeClass('backup').addClass('diva');
+      $('#caro-6-link').removeClass('diva').addClass('backup');
       $('#pos6').removeClass('diva');
       $('#pos1').addClass('diva');
     }
@@ -53,31 +65,43 @@ $(function() {
     if ($(toPop).attr('id') === 'caro-6') {
       $(toPop).prev().removeClass('backup').addClass('diva');
       $(toPop).removeClass('diva').addClass('backup');
+      $('#caro-5-link').removeClass('backup').addClass('diva');
+      $('#caro-6-link').removeClass('diva').addClass('backup');
       $('#pos6').removeClass('diva');
       $('#pos5').addClass('diva');
     } else if ($(toPop).attr('id') === 'caro-5') {
       $(toPop).prev().removeClass('backup').addClass('diva');
       $(toPop).removeClass('diva').addClass('backup');
+      $('#caro-4-link').removeClass('backup').addClass('diva');
+      $('#caro-5-link').removeClass('diva').addClass('backup');
       $('#pos5').removeClass('diva');
       $('#pos4').addClass('diva');
     } else if ($(toPop).attr('id') === 'caro-4') {
       $(toPop).prev().removeClass('backup').addClass('diva');
       $(toPop).removeClass('diva').addClass('backup');
+      $('#caro-3-link').removeClass('backup').addClass('diva');
+      $('#caro-4-link').removeClass('diva').addClass('backup');
       $('#pos4').removeClass('diva');
       $('#pos3').addClass('diva');
     } else if ($(toPop).attr('id') === 'caro-3') {
       $(toPop).prev().removeClass('backup').addClass('diva');
       $(toPop).removeClass('diva').addClass('backup');
+      $('#caro-2-link').removeClass('backup').addClass('diva');
+      $('#caro-3-link').removeClass('diva').addClass('backup');
       $('#pos3').removeClass('diva');
       $('#pos2').addClass('diva');
     } else if ($(toPop).attr('id') === 'caro-2') {
       $(toPop).prev().removeClass('backup').addClass('diva');
       $(toPop).removeClass('diva').addClass('backup');
+      $('#caro-1-link').removeClass('backup').addClass('diva');
+      $('#caro-2-link').removeClass('diva').addClass('backup');
       $('#pos2').removeClass('diva');
       $('#pos1').addClass('diva');
     } else {
       $(this).siblings('#caro-6').removeClass('backup').addClass('diva');
       $(this).siblings('#caro-1').removeClass('diva').addClass('backup');
+      $('#caro-6-link').removeClass('backup').addClass('diva');
+      $('#caro-1-link').removeClass('diva').addClass('backup');
       $('#pos1').removeClass('diva');
       $('#pos6').addClass('diva');
     }
@@ -86,36 +110,48 @@ $(function() {
   $('#carousel').on('click', 'div#pos1', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-1-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-1').removeClass('backup').addClass('diva');
   });
   $('#carousel').on('click', 'div#pos2', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-2-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-2').removeClass('backup').addClass('diva');
   });
   $('#carousel').on('click', 'div#pos3', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-3-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-3').removeClass('backup').addClass('diva');
   });
   $('#carousel').on('click', 'div#pos4', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-4-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-4').removeClass('backup').addClass('diva');
   });
   $('#carousel').on('click', 'div#pos5', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-5-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-5').removeClass('backup').addClass('diva');
   });
   $('#carousel').on('click', 'div#pos6', function() {
     $('.caro-item.diva').addClass('backup').removeClass('diva');
     $('.pos.diva').removeClass('diva');
+    $('.caro-link.diva').removeClass('diva');
+    $('#caro-6-link').removeClass('backup').addClass('diva');
     $(this).addClass('diva');
     $('#caro-6').removeClass('backup').addClass('diva');
   });
